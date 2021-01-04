5008de29fae5a4c5efca715ff12c43a0c9b4ed93
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2021-01-04 15:22:31 -0500

    findlongestword

--- FindLongestWord.js
+++ FindLongestWord.js
@@ -1,6 +1,13 @@
-function findLongestWordLength(str) {
+
   
-  return str.length;
+  // Split the string into an array
+  // Then sort the array by its elements length, from longest to shortest
+  // Since the first element of the array will be the longest, then return the length of the first element
+
+const findLongestWord = str => {
+
+  const longestWord = str.split('').sort(function (a, b) { return b.length - a.length; });
+  return longestWord[0];
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+findLongestWord("The quick brown fox jumped over the lazy dog");bv 
